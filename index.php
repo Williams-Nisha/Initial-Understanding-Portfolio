58c913e65388248e47e72bdb6a847cc08bbb826f
Nisha <Nisha@192.168.1.103>
2015-05-09 15:41:57 -0600

    New file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+ <meta charset="utf-8">
+ <meta name="description" content="This section of my portfolio shows examples of my coding experience in my 261 class." >
+ <meta  name="viewport" content="width=device-width, initial-scale=1.0"> 
+ <title>Coding Portfolio</title>
+ <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+ <link type="text/css" media="screen" href="css/styles.css" rel="stylesheet">
+ </head>
+ <body>
+<div class="container">
+     <header>
+         <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php';?>
+     </header>
+     <main>
+         <h1>Coding Portfolio</h1>
+
+    <article>
+    <h2>Coding Subjets</h2>
+    <ul class="pad">
+        <li>The software development process and some standard methodologies</li>
+        <li>Creative, pleasant, mobile user interface creation</li>
+        <li><a href="/javascript.php">JavaScript objects</a></li>
+        <li><a href="/dom_manipulation.php">Dom Manipulations</a></li>
+        <li>CSS3 transitions</li>
+        <li>CSS3 animations</li>
+        <li>Using local data storage</li>
+        <li>AJAX interactions with remote services</li>
+        <li>JSON data serialization</li>
+    </ul>
+    </article> 
+     </main>
+     <footer>
+                  <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php';?>
+     </footer>
+</div>
+ </body>
+</html>
